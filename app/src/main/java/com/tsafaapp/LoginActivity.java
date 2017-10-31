47e8c36e3c495991958ed8bed82d1ebfdf51b01d
whaaazaaap1996@gmail.com <2310906863depraile>
2017-11-01 01:47:56 +0200

    change log sindesi firebase login

---
+++ app/src/main/java/com/tsafaapp/LoginActivity.java
@@ -0,0 +1,78 @@
+package com.tsafaapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText etmainemaillogin = (EditText) findViewById(R.id.etmainemaillogin);
+        final EditText etmainpasswordlogin = (EditText) findViewById(R.id.etmainpasswordlogin);
+        final CardView cvlogin = (CardView) findViewById(R.id.cvlogin);
+        final TextView signup = (TextView) findViewById(R.id.signup);
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser()!=null){
+            finish();
+            startActivity(new Intent(getApplicationContext(),UserActivity.class));
+        }
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent signupIntent = new Intent(LoginActivity.this,RegisterActivity.class);
+                LoginActivity.this.startActivity(signupIntent);
+            }
+        });
+
+
+        cvlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String password=etmainpasswordlogin.getText().toString().trim();
+                String email=etmainemaillogin.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(LoginActivity.this, "Please enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(LoginActivity.this, "Please enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            finish();
+                            startActivity(new Intent(getApplicationContext(),UserActivity.class));
+                        }
+
+                    }
+                });
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/tsafaapp/RegisterActivity.java
@@ -0,0 +1,100 @@
+package com.tsafaapp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+    // private ProgressDialog progressDialog;
+     private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+            firebaseAuth =firebaseAuth.getInstance();
+
+         final EditText etname = (EditText) findViewById(R.id.etname);
+         final EditText etsurname = (EditText) findViewById(R.id.etsurname);
+         final EditText etusernamelogin = (EditText) findViewById(R.id.etusernamelogin);
+         final EditText etpasswordlogin = (EditText) findViewById(R.id.etpasswordlogin);
+        // EditText etrenterpassword = (EditText) findViewById(R.id.etrenterpassword);
+         final EditText etemail = (EditText) findViewById(R.id.etemail);
+         CardView cvregister = (CardView) findViewById(R.id.cardView);
+
+        if(firebaseAuth.getCurrentUser()!=null){
+            finish();
+            startActivity(new Intent(getApplicationContext(),UserActivity.class));
+        }
+
+        cvregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=etname.getText().toString().trim();
+                String surname=etsurname.getText().toString().trim();
+                String username=etusernamelogin.getText().toString().trim();
+                String password=etpasswordlogin.getText().toString().trim();
+                String email=etemail.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(RegisterActivity.this, "Please enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(RegisterActivity.this, "Please enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(name)){
+                    Toast.makeText(RegisterActivity.this, "Please enter name", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(surname)){
+                    Toast.makeText(RegisterActivity.this, "Please enter surname", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(username)) {
+                    Toast.makeText(RegisterActivity.this, "Please enter username", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+
+                firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                            finish();
+                            startActivity(new Intent(getApplicationContext(),UserActivity.class));
+                        }else{
+                            Toast.makeText(RegisterActivity.this, "Could not register,please try again", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+               // progressDialog.setMessage("Registering User...");
+              //  progressDialog.show();
+            }
+        });
+
+
+
+    }
+
+}
