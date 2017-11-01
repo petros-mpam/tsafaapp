b26f79945e406a4f24197db0ec183827288a7e67
thanasismademlis <athamade1@teicm.gr>
2017-11-01 22:54:40 +0200

    shopper regster

--- app/src/main/java/com/tsafaapp/ShopperLoginActivity.java
+++ app/src/main/java/com/tsafaapp/ShopperLoginActivity.java
@@ -1,7 +1,10 @@
 package com.tsafaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ShopperLoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class ShopperLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopper_login);
+
+       final TextView shopsignup = (TextView) findViewById(R.id.shopsignup);   //or sign up
+
+        shopsignup.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent shopsignupIntent = new Intent(ShopperLoginActivity.this,ShopperRegisterActivity.class);
+                ShopperLoginActivity.this.startActivity(shopsignupIntent);
+            }
+        });
+
+
     }
 }
