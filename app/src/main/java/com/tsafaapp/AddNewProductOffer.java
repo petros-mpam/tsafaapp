c883c8c012ef1ac7a976a67cf05404848d61ed1f
thanasismademlis <athamade1@teicm.gr>
2017-11-06 11:33:16 +0200

    EditTexts sto Add New Product Offer

--- app/src/main/java/com/tsafaapp/AddNewProductOffer.java
+++ app/src/main/java/com/tsafaapp/AddNewProductOffer.java
@@ -2,6 +2,7 @@ package com.tsafaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AddNewProductOffer extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class AddNewProductOffer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_product_offer);
+
+        final EditText etCategory = (EditText) findViewById(R.id.etCategory);
+        final EditText etProduct = (EditText) findViewById(R.id.etProduct);
+        final EditText etInitialValue = (EditText) findViewById(R.id.etInitialValue);
+        final EditText etOfferValue = (EditText) findViewById(R.id.etOfferValue);
+
     }
 }
