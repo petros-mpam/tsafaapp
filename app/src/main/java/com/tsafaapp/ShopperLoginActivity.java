ef0f9a5dd367228fee8ad5a62b8db04456da4e67
thanasismademlis <athamade1@teicm.gr>
2017-11-13 20:56:51 +0200

    shopperlogin in progress yet

---
+++ app/src/main/java/com/tsafaapp/ShopperLoginActivity.java
@@ -0,0 +1,13 @@
+package com.tsafaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShopperLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopper_login);
+    }
+}
