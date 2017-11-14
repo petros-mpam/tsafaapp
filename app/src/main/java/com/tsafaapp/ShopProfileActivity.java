ecd3050f5ac90fae81ad7e33baa66c50e73b84d7
thanasismademlis <athamade1@teicm.gr>
2017-11-14 11:12:11 +0200

    shop's add new offer

--- app/src/main/java/com/tsafaapp/ShopProfileActivity.java
+++ app/src/main/java/com/tsafaapp/ShopProfileActivity.java
@@ -1,7 +1,10 @@
 package com.tsafaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class ShopProfileActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class ShopProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_profile);
+
+        final CardView cardViewaddnewoffer = (CardView) findViewById(R.id.cardViewaddnewoffer);
+
+        cardViewaddnewoffer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent shopAddOfferIntent = new Intent(ShopProfileActivity.this,AddNewProductOfferActivity.class);
+                ShopProfileActivity.this.startActivity(shopAddOfferIntent);
+            }
+        });
+
     }
 }
