c3b051f80fae04fb9d6a70fa9cb8dc1a733282e5
thanasismademlis <athamade1@teicm.gr>
2017-11-14 10:51:45 +0200

    shop's register activity

---
+++ app/src/main/java/com/tsafaapp/ShopperRegisterActivity.java
@@ -0,0 +1,18 @@
+package com.tsafaapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ShopperRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopper_register);
+
+
+    }
+}
