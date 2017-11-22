e15bd43bc66e5cfce3c43bbc947a6f085ff76b83
WHAAAZAAAP <5690526+WHAAAZAAAP@users.noreply.github.com>
2017-11-22 19:10:47 +0200

    bugfix2

---
+++ app/src/main/java/com/tsafaapp/PelatisData.java
@@ -0,0 +1,59 @@
+package com.tsafaapp;
+
+/**
+ * Created by WHAAAZAAAP on 14/11/2017.
+ */
+
+public class PelatisData {
+    String id,name,surname,username,email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public PelatisData(String id,String name, String surname, String username, String email){
+        this.id=id;
+        this.name=name;
+        this.surname=surname;
+        this.username=username;
+        this.email=email;
+
+    }
+
+}
