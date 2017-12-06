dbce3e81872f2c54802177111042ad54938e227d
Teo <teopal95@hotmailcom>
2017-12-06 02:17:23 +0200

    menu test

--- app/src/main/java/com/tsafaapp/UserActivity.java
+++ app/src/main/java/com/tsafaapp/UserActivity.java
@@ -3,9 +3,12 @@ package com.tsafaapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -16,13 +19,49 @@ import java.util.ArrayList;
 
 public class UserActivity extends AppCompatActivity {
 
-    private FirebaseAuth firebaseAuth;
+    String[] c_names = { "women","men","bags","watches","jewls","sunglasses","accessories","kids"};
+
+    int [] a_items = {R.drawable.women1,R.drawable.men1,R.drawable.bags1,R.drawable.watches1,R.drawable.jewls1,
+            R.drawable.sunglasses1 ,R.drawable.accessories1,R.drawable.kids1};
+
+
+    android.support.v7.widget.Toolbar toolbar;
+
+    RecyclerView recyclerView;
+    RecyclerAdapter adapter;
+
+    RecyclerView.LayoutManager layoutManager;
+
+    ArrayList<Items> arrayList = new ArrayList<>();
+
+
+
+    //private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-        TextView tvemail=(TextView)findViewById(R.id.tvemail);
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        int count = 0;
+        for (String Name : c_names)
+        {
+            arrayList.add(new Items(Name,a_items[count]));
+            count++;
+        }
+
+        adapter = new RecyclerAdapter(arrayList);
+        recyclerView.setAdapter(adapter);
+
+
+      /*  TextView tvemail=(TextView)findViewById(R.id.tvemail);
         Button bcshop=(Button)findViewById(R.id.bcshop);
         Button blogout=(Button)findViewById(R.id.bLogout1);
 
@@ -76,11 +115,13 @@ public class UserActivity extends AppCompatActivity {
         });
 
 
-
+*/
 
 
 
 
 
     }
+
+
 }
