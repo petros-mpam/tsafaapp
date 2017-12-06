0e53bc00abe72f6064b82b150d3eead237f6152e
thanasismademlis <athamade1@teicm.gr>
2017-12-06 12:57:18 +0200

    created a simple menu_tryna create multi list now

---
+++ app/src/main/java/com/tsafaapp/Shoppers_ItemsList.java
@@ -0,0 +1,13 @@
+package com.tsafaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Shoppers_ItemsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoppers__items_list);
+    }
+}
