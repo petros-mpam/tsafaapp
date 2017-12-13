eaf9ee5e22fc17322f11a349ec3419467fab694a
Teo <teopal95@hotmailcom>
2017-12-13 12:58:03 +0200

    bugfix 4

--- app/src/main/java/com/tsafaapp/RecyclerAdapter.java
+++ app/src/main/java/com/tsafaapp/RecyclerAdapter.java
@@ -13,3 +13,51 @@ import java.util.ArrayList;
  * Created by Teo on 12/11/2017.
  */
 
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+
+    ArrayList<Items> arrayList = new ArrayList<>();
+    RecyclerAdapter(ArrayList<Items> arrayList)
+    {
+        this.arrayList = arrayList;
+    }
+
+
+
+
+
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_layout,parent,false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        holder.c_flag.setImageResource(arrayList.get(position).getFlag_id());
+        holder.c_name.setText(arrayList.get(position).getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder
+    {
+
+        ImageView c_flag;
+        TextView c_name;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            c_flag = (ImageView) itemView.findViewById(R.id.flag);
+            c_name = (TextView) itemView.findViewById(R.id.name);
+        }
+    }
+}
